b51e286c577205843d5dc05d7394090fa7e5305a
anilcodersigdel <anilsigdeel594@gmail.com>
2024-08-30 14:04:08 +0545

    basic syntax for fetch api from promise and async await

---
+++ main.js
@@ -0,0 +1,31 @@
+ const URL = "https://cat-fact.herokuapp.com/facts";
+const paraFacts = document.querySelector('#facts');
+const factButton =  document.querySelector('#Btn');
+
+//  let promise = fetch(URL);     Normal syntax for fetch api 
+//  console.log(promise);
+
+// const getFacts = async() => {
+//     let response = await fetch(URL);
+//     let json = await response.json();         
+//     paraFacts.innerText = json[0].text;
+// }
+
+// By Using promise 
+
+function getFacts(){
+    fetch(URL)
+
+    .then((response) => {
+        return response.json();
+    })
+    .then((json) => {
+        console.log(json);
+        paraFacts.innerText = json[0].text;
+    })
+    .catch((error)=> {
+        console.log(error);
+    })
+}
+
+factButton.addEventListener('click', getFacts);
